19ea4deb549569ba05783ce455fc1db4fdad6dbc
Volohovich Denis <sined07@yandex.by>
2021-01-26 11:15:15 +0300

    add HelloWorld

---
+++ TMS.Net07.Homework.HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TMS.Net07.Homework.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string value;
+            Console.WriteLine("To exit from application enter 0.");
+            while (true) {                
+                Console.WriteLine("Please, enter integer value:");
+                value = Console.ReadLine();
+                bool ok = int.TryParse(value, out number);
+                string msg = ok ? "You enter integer: " + number.ToString() : "You enter: " + value + ". This is no integer! Try again.";
+                Console.WriteLine(msg);
+                if (ok && number == 0)
+                {
+                    Console.WriteLine("Exit from application.");
+                    System.Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
