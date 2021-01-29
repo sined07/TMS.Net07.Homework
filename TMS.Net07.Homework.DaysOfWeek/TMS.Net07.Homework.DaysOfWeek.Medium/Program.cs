4217bc8afb14f3c02b36b2e041c65ad7a5f68f36
sined07 <sined07@yandex.ru>
2021-01-29 09:58:06 +0300

    refactor: medium level

--- TMS.Net07.Homework.DaysOfWeek/TMS.Net07.Homework.DaysOfWeek.Medium/Program.cs
+++ TMS.Net07.Homework.DaysOfWeek/TMS.Net07.Homework.DaysOfWeek.Medium/Program.cs
@@ -3,20 +3,17 @@ using System.Globalization;
 
 namespace TMS.Net07.Homework.DaysOfWeek
 {
-    //DateTime.MinValue Field = "01.01.0001"  
-    //DateTime.MaxValue Field = "01.01.10000" 
+
     class Program
     {
         const string datePattern = "dd.MM.yyyy";
-        const string stringDateMin = "01.01.0001";
-        const string stringDateMax = "31.12.2999";
-        private static DateTime dateValue = DateTime.Now;
+        const string dateStringMin = "01.01.0001";
+        const string dateStringMax = "31.12.2999";
 
         static void Main(string[] args)
         {
-            
             Console.WriteLine("To exit from application enter 'exit'.");
-            Console.WriteLine("Valid date range: " + stringDateMin + " - " + stringDateMax);
+            Console.WriteLine("Valid date range: " + dateStringMin + " - " + dateStringMax);
             while (true)
             {
                 Console.WriteLine("Please, enter date use format " + datePattern + " (e.g. 01.01.2021):");
@@ -25,32 +22,27 @@ namespace TMS.Net07.Homework.DaysOfWeek
                 {
                     return;
                 }
-                if (IsValidDate(value))
-                {
-                    Console.WriteLine(GetDayOfWeek());
-                    continue;
-                }
-                Console.WriteLine("Entered value is not valid date.");
-            } 
-
-        }
-
-        private static bool IsValidDate(string dateString)
-        {
-            bool isValid = DateTime.TryParseExact(dateString, datePattern, CultureInfo.CurrentCulture, DateTimeStyles.None, out dateValue);
-            if (isValid) 
-            {                
-                isValid = (dateValue.Ticks <= DateTime.Parse(stringDateMax).Ticks);
+                Console.WriteLine(GetDayOfWeek(value));
             }
-            return isValid;
         }
 
-        private static string GetDayOfWeek()
+        private static string GetDayOfWeek(string dateString)
         {
             CultureInfo ru = new CultureInfo("ru-RU");
+            bool isValid = DateTime.TryParseExact(dateString, datePattern, ru, DateTimeStyles.None, out DateTime dateValue);
+            if (isValid)
+            {
+                // check is valid range datetime
+                isValid = dateValue.Ticks <= DateTime.Parse(dateStringMax).Ticks &&
+                    dateValue.Ticks >= DateTime.Parse(dateStringMin).Ticks;
+            }
+            if (!isValid)
+            {
+                return "Entered value is not valid date.";
+            }
             string dayOfWeek = dateValue.ToString("dddd", ru);
-            return char.ToUpper(dayOfWeek[0]) + dayOfWeek.Substring(1);
+            dayOfWeek = char.ToUpper(dayOfWeek[0]) + dayOfWeek.Substring(1);
+            return dayOfWeek;
         }
     }
-
 }
