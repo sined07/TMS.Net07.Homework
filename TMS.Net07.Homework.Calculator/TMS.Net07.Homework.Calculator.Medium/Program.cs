6c925e60721280bddf2cf5c80d7a40e18699b9cd
sined07 <sined07@yandex.ru>
2021-02-04 10:40:17 +0300

    refactor: medium level

--- TMS.Net07.Homework.Calculator/TMS.Net07.Homework.Calculator.Medium/Program.cs
+++ TMS.Net07.Homework.Calculator/TMS.Net07.Homework.Calculator.Medium/Program.cs
@@ -4,10 +4,12 @@ namespace TMS.Net07.Homework.Calculator.Medium
 {
     class Program
     {
+        private static string[] operators = { "+", "-", "*", "/", "%" };
+
         static void Main(string[] args)
         {
             Console.WriteLine("This is simple calculator.");
-            Console.WriteLine("Supported operations: +, -, *, /, %");
+            Console.WriteLine("Supported operations: " + "[{0}]", string.Join(", ", operators));
             while (true)
             {
                 string[] inputValues = GetInputValues();
@@ -15,7 +17,7 @@ namespace TMS.Net07.Homework.Calculator.Medium
                 Console.WriteLine("Try again? (y/n):");
                 if (!Console.ReadLine().Trim().ToLower().StartsWith('y'))
                 {
-                    return;
+                    break;
                 }
             }
         }
@@ -44,18 +46,19 @@ namespace TMS.Net07.Homework.Calculator.Medium
             {
                 return "Entered number is not valid!";
             }
+            string prefix = "result: ";
             switch (inputValues[2])
             {
                 case "+":
-                    return ("result: " + (num1 + num2));
+                    return (prefix + (num1 + num2));
                 case "-":
-                    return ("result: " + (num1 - num2));
+                    return (prefix + (num1 - num2));
                 case "*":
-                    return ("result: " + (num1 * num2));
+                    return (prefix + (num1 * num2));
                 case "/":
-                    return (num2 == 0) ? "Error - divide by 0!" : ("result: " + (num1 / num2));
+                    return (num2 == 0) ? "Error - divide by 0!" : (prefix + (num1 / num2));
                 case "%":
-                    return (num2 == 0) ? "Error - remainder by 0!" : ("result: " + (num1 % num2));
+                    return (num2 == 0) ? "Error - remainder by 0!" : (prefix + (num1 % num2));
             }
 
             return "Unsupported operation!";
