0e75772a1d2adfe67d9867617be3e9038b761fb1
sined07 <sined07@yandex.ru>
2021-02-05 15:35:49 +0300

    feat(converter): get rate from API

--- TMS.Net07.Homework.Calculator/TMS.Net07.Homework.Calculator.Light/Program.cs
+++ TMS.Net07.Homework.Calculator/TMS.Net07.Homework.Calculator.Light/Program.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Text.Json;
 
 namespace TMS.Net07.Homework.Calculator.Light
 {
     class Program
     {
-
         private static Dictionary<string, double> currenciesDictionary = new Dictionary<string, double>();
+        private const string outputFormat = "{0:0.0000}";
 
         static void Main(string[] args)
         {
-            InitCurrenciesDictionary();
+            InitDictionaryFromAPI();
             while (true)
             {
                 string[] inputValues = GetInputValues();
                 double amount = GetOutputAmount(inputValues);
                 Console.WriteLine(amount < 0 ? "You entered is not valid value!" :
-                                  $"{inputValues[2]} {inputValues[0]} is equal to {amount} {inputValues[1]}");
+                $"{inputValues[2]} {inputValues[0]} is equal to {string.Format(outputFormat, amount)} {inputValues[1]}");
                 Console.WriteLine("Try again? (y/n):");
                 if (!Console.ReadLine().Trim().ToLower().StartsWith('y'))
                 {
@@ -25,14 +28,77 @@ namespace TMS.Net07.Homework.Calculator.Light
             }
         }
 
-        private static void InitCurrenciesDictionary()
+        private static void InitDictionaryFromAPI()
+        {
+            String jsonRate = GetJsonRateFromAPI();
+            List<CurrencyRate> listRate = null;
+            try
+            {
+                listRate = JsonSerializer.Deserialize<List<CurrencyRate>>(jsonRate);
+            }
+            catch (JsonException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (listRate == null || listRate.Count == 0)
+            {
+                InitDictionaryFromHistory();
+                return;
+            }
+
+            //add rates in currencies dictionary
+            currenciesDictionary.Add("BYN", 1);
+            String dateRate = "";
+            foreach (CurrencyRate rate in listRate)
+            {
+                dateRate = rate.Date.ToString("dd-MM-yyyy");
+                double scaleRate = Math.Round(rate.Cur_OfficialRate / rate.Cur_Scale, 4);
+                currenciesDictionary.Add(rate.Cur_Abbreviation, scaleRate);
+            }
+            PrintRateInfo(dateRate);
+        }
+
+        private static string GetJsonRateFromAPI()
+        {
+            WebResponse response = null;
+            String jsonRate = "";
+            try
+            {
+                WebRequest request = WebRequest.Create("https://www.nbrb.by/api/exrates/rates?periodicity=0");
+                response = request.GetResponse();
+                using Stream stream = response.GetResponseStream();
+                using StreamReader reader = new StreamReader(stream);
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    jsonRate += line;
+                }
+
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message);
+                return "";
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+            return jsonRate;
+        }
+
+        private static void InitDictionaryFromHistory()
         {
             string dateRate = "03-02-2021";
-            string[] code = { "BYN", "EUR", "RUB", "USD" };
-            double[] rate = { 1.0, 3.1762, 0.034670, 2.6293 };
-            for (int i = 0; i < code.Length; i++)
+            string[] codes = { "BYN", "EUR", "RUB", "USD" };
+            double[] rates = { 1.0, 3.1762, 0.034670, 2.6293 };
+            for (int i = 0; i < codes.Length; i++)
             {
-                currenciesDictionary.Add(code[i], rate[i]);
+                currenciesDictionary.Add(codes[i], rates[i]);
             }
             PrintRateInfo(dateRate);
         }
@@ -42,7 +108,7 @@ namespace TMS.Net07.Homework.Calculator.Light
             Console.WriteLine($"Official Exchange Rate of the Belarusian Ruble at {dateRate}:");
             foreach (KeyValuePair<string, double> entry in currenciesDictionary)
             {
-                Console.WriteLine(entry.Key + ": " + entry.Value);
+                Console.WriteLine(entry.Key + ": " + string.Format(outputFormat, entry.Value));
             }
         }
 
@@ -74,5 +140,15 @@ namespace TMS.Net07.Homework.Calculator.Light
             double amountTarget = (rateSource / rateTarget) * amountSource;
             return Math.Round(amountTarget, 4);
         }
+    }
+
+    public class CurrencyRate
+    {
+        public int Cur_ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Cur_Abbreviation { get; set; }
+        public int Cur_Scale { get; set; }
+        public string Cur_Name { get; set; }
+        public double Cur_OfficialRate { get; set; }
     }
 }
