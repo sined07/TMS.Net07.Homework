69bad7e7ffb719a3b6d93bea59d45fdcd50644cc
sined07 <sined07@yandex.ru>
2021-02-10 16:35:22 +0300

    add(shape): light level

---
+++ TMS.Net07.Homework.Shapes/TMS.Net07.Homework.Shapes.Light/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TMS.Net07.Homework.Shapes.Light
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+        }
+
+        public static void Test()
+        {
+            Shape[] shapes = {
+            new Point(15.1025, 10.478),
+            new Rectangle(new Point(17.54, -14.23), 10.54, 20.111),
+            new Square(new Point(0, 0), 19.23),
+            new Ellipse(new Point(0, 0), 15.23, 0.4),
+            new Circle(new Point(45.26, 51), 7.393)
+            };
+            foreach (global::Shape shape in shapes)
+            {
+                Console.WriteLine($"Area = {shape.GetArea()}");
+            }
+        }
+    }
+}
