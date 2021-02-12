69b8185520121b168647a4c64f23246eee4b1342
sined07 <sined07@yandex.ru>
2021-02-12 17:00:20 +0300

    add: draft resolve hard level

--- TMS.Net07.Homework.Shapes/TMS.Net07.Homework.Shapes.Hard/ConsoleCommandDrawer.cs
+++ TMS.Net07.Homework.Shapes/TMS.Net07.Homework.Shapes.Hard/ConsoleCommandDrawer.cs
@@ -5,6 +5,8 @@ public class ConsoleCommandDrawer
 {
 
     private List<Shape> listShape = new List<Shape>();
+    private ConsoleDescriptionDrawer logger = new ConsoleDescriptionDrawer();
+    private ConsoleRenderDrawer drawer = new ConsoleRenderDrawer();
 
     public ConsoleCommandDrawer() 
     {
@@ -20,12 +22,79 @@ public class ConsoleCommandDrawer
         listShape.Add(new Circle(new Point(45, 100), 80));
     }
 
-    public void SetCommand(string command) 
-    {
-    
+
+    public void ExecuteCommand(string command)
+    {        
+        switch (command)
+        {            
+            case string _ when command.Contains("log"):
+                PrintLog();
+                break;
+            case string _ when command.Contains("draw"):
+                DrawShape(command);                
+                break;
+            case string _ when command.Contains("triangle"):
+                CreateTriangle(command);
+                break;
+            case string _ when command.Contains("rectangle"):
+                CreateRectangle(command);
+                break;
+            case string _ when command.Contains("square"):
+                CreateSquare(command);
+                break;
+            case string _ when command.Contains("ellipse"):
+                CreateEllipse(command);
+                break;            
+            case string _ when command.Contains("circle"):
+                CreateCircle(command);
+                break;
+            default:
+                Console.WriteLine("Unrecognised command!");
+                return;
+        }
     }
     
 
+    private void CreateRectangle(string command)
+    {
+    }
+
+    private void CreateSquare(string command)
+    {
+    }
+
+    private void CreateEllipse(string command)
+    {
+    }
+
+    private void CreateCircle(string command)
+    {
+    }
+   
+
+    private void CreateTriangle(string command)
+    {
+    }
 
 
+
+    private void DrawShape(string command) 
+    {
+        command = command.Replace("draw", "").Trim();
+        bool ok = int.TryParse(command, out int index);
+        index = (index < 0) ? listShape.Count + index : index - 1;
+        if (!ok || index < 0 || index >= listShape.Count)
+        {
+            Console.WriteLine("The shape with the specified index does not exist!");
+            return;
+        }
+        drawer.Draw(listShape[index]);
+    }
+
+    private void PrintLog() {
+        foreach (Shape shape in listShape)
+        {
+            logger.Draw(shape);
+        }    
+    }
 }
--- TMS.Net07.Homework.Shapes/TMS.Net07.Homework.Shapes.Hard/Program.cs
+++ TMS.Net07.Homework.Shapes/TMS.Net07.Homework.Shapes.Hard/Program.cs
@@ -5,8 +5,15 @@ namespace TMS.Net07.Homework.Shapes.Hard
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {           
+            string command1 = "log";
+            string command2 = "draw5";
+            string command3 = "draw -3";
+
+            ConsoleCommandDrawer drawer = new ConsoleCommandDrawer();
+            drawer.ExecuteCommand(command1);
+            drawer.ExecuteCommand(command2);
+            drawer.ExecuteCommand(command3);
         }
     }
 }
