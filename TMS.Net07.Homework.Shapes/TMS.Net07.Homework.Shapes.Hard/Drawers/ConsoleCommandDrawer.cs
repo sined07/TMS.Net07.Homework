a9e25bbee60dd03f91b9cfb553748668b3aa8690
sined07 <sined07@yandex.ru>
2021-02-17 22:49:00 +0300

    refactor: naming variables,  test

--- TMS.Net07.Homework.Shapes/TMS.Net07.Homework.Shapes.Hard/Drawers/ConsoleCommandDrawer.cs
+++ TMS.Net07.Homework.Shapes/TMS.Net07.Homework.Shapes.Hard/Drawers/ConsoleCommandDrawer.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 
 public class ConsoleCommandDrawer
 {
-
-    private List<Shape> listShape = new List<Shape>();
-    private ConsoleDescriptionDrawer logger = new ConsoleDescriptionDrawer();
-    private ConsoleRenderDrawer drawer = new ConsoleRenderDrawer();
-    private bool isWork = true;
-    private readonly string[,] commands = new string[,]
+    private List<Shape> _listShape = new List<Shape>();
+    private ConsoleDescriptionDrawer _logger = new ConsoleDescriptionDrawer();
+    private ConsoleRenderDrawer _drawer = new ConsoleRenderDrawer();
+    private bool _isWork = true;
+    private readonly string[,] _commands = new string[,]
     {
-        { "help", "help" },
         { "exit", "exit" },
+        { "help", "help" },
         { "log", "log" },
         { "draw", "draw 1"},
         { nameof(Square).ToLower(), "square (30,50) 15" },
@@ -24,7 +23,7 @@ public class ConsoleCommandDrawer
     public void Init()
     {
         PrintHelp();
-        while (isWork)
+        while (_isWork)
         {
             Console.WriteLine("Enter command:");
             ExecuteCommand(Console.ReadLine().ToLower());
@@ -38,45 +37,45 @@ public class ConsoleCommandDrawer
         int index = -1;
         switch (command)
         {
+            // command exit
+            case string _ when command.Contains(_commands[++index, 0]):
+                _isWork = false;
+                return;
             // command help
-            case string _ when command.Contains(commands[++index, 0]):
+            case string _ when command.Contains(_commands[++index, 0]):
                 PrintHelp();
                 return;
-            // command exit
-            case string _ when command.Contains(commands[++index, 0]):
-                isWork = false;
-                return;
             // command log
-            case string _ when command.Contains(commands[++index, 0]):
+            case string _ when command.Contains(_commands[++index, 0]):
                 PrintLog();
                 return;
             // command draw
-            case string _ when command.Contains(commands[++index, 0]):
-                args = GetIntArgs(command, commands[index, 0]);
+            case string _ when command.Contains(_commands[++index, 0]):
+                args = GetIntArgs(command, _commands[index, 0]);
                 DrawShape(args);
                 return;
             // command square
-            case string _ when command.Contains(commands[++index, 0]):
-                args = GetIntArgs(command, commands[index, 0]);
+            case string _ when command.Contains(_commands[++index, 0]):
+                args = GetIntArgs(command, _commands[index, 0]);
                 shape = (args == null || args.Length != 3) ? null :
                         new Square(new Point(args[0], args[1]), args[2]);
                 break;
             // command circle
-            case string _ when command.Contains(commands[++index, 0]):
-                args = GetIntArgs(command, commands[index, 0]);
+            case string _ when command.Contains(_commands[++index, 0]):
+                args = GetIntArgs(command, _commands[index, 0]);
                 shape = (args == null || args.Length != 3) ? null :
                         new Circle(new Point(args[0], args[1]), args[2]);
                 break;
             // command ellipse
-            case string _ when command.Contains(commands[++index, 0]):
-                args = GetIntArgs(command, commands[index, 0]);
+            case string _ when command.Contains(_commands[++index, 0]):
+                args = GetIntArgs(command, _commands[index, 0]);
                 shape = (args == null || args.Length != 4) ? null :
                         new Ellipse(new Point(args[0], args[1]),
                         args[2], args[3]);
                 break;
             // command triangle
-            case string _ when command.Contains((commands[++index, 0])):
-                args = GetIntArgs(command, commands[index, 0]);
+            case string _ when command.Contains((_commands[++index, 0])):
+                args = GetIntArgs(command, _commands[index, 0]);
                 shape = (args == null || args.Length != 6) ? null :
                         new Triangle(
                         new Point(args[0], args[1]),
@@ -84,8 +83,8 @@ public class ConsoleCommandDrawer
                         new Point(args[4], args[5]));
                 break;
             // command rectangle
-            case string _ when command.Contains(commands[++index, 0]):
-                args = GetIntArgs(command, commands[index, 0]);
+            case string _ when command.Contains(_commands[++index, 0]):
+                args = GetIntArgs(command, _commands[index, 0]);
                 shape = (args == null || args.Length != 4) ? null :
                         new Rectangle(
                         new Point(args[0], args[1]),
@@ -122,27 +121,27 @@ public class ConsoleCommandDrawer
             return;
         }
         int index = args[0];
-        index = (index < 0) ? listShape.Count + index : index - 1;
-        if (index < 0 || index >= listShape.Count)
+        index = (index < 0) ? _listShape.Count + index : index - 1;
+        if (index < 0 || index >= _listShape.Count)
         {
             Console.WriteLine("The shape with the specified index does not exist!");
             return;
         }
-        drawer.Draw(listShape[index]);
+        _drawer.Draw(_listShape[index]);
     }
 
     private void PrintLog()
     {
-        if (listShape.Count == 0)
+        if (_listShape.Count == 0)
         {
             Console.WriteLine("Log is empty!");
             return;
         }
 
         int i = 0;
-        foreach (Shape shape in listShape)
+        foreach (Shape shape in _listShape)
         {
-            logger.Draw($"{ ++i}. ", shape);
+            _logger.Draw($"{ ++i}. ", shape);
         }
     }
 
@@ -153,26 +152,25 @@ public class ConsoleCommandDrawer
             Console.WriteLine("Unrecognised shape args!");
             return;
         }
-        listShape.Add(shape);
-        drawer.Draw(shape);
+        _listShape.Add(shape);
+        _drawer.Draw(shape);
     }
 
     private void PrintHelp()
     {
         Console.WriteLine("Supported commands:");
-        for (int i = 0; i < commands.GetLength(0); i++)
+        for (int i = 0; i < _commands.GetLength(0); i++)
         {
-            string tab = commands[i, 0].Length < 8 ? "\t\t" : "\t";
-            Console.WriteLine($"{commands[i, 0]}{tab}example usage: {commands[i, 1]}");
-
+            string tab = _commands[i, 0].Length < 8 ? "\t\t" : "\t";
+            Console.WriteLine($"{_commands[i, 0]}{tab}example usage: {_commands[i, 1]}");
         }
     }
 
     private void Test()
     {
-        for (int i = 0; i < commands.GetLength(0); i++)
+        for (int i = _commands.GetLength(0) - 1; i >= 0; i--)
         {
-            string command = commands[i, 1];
+            string command = _commands[i, 1];
             Console.WriteLine("Execute command: " + command);
             ExecuteCommand(command);
         }
